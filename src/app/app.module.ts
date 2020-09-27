89e3d6943ef0ff11b860c71d965a4b281fcc2f18
riya-sharma-17 <riya17599sharma@gmail.com>
2020-09-27 21:33:24 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import '@angular/compiler';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MaincontentComponent
   ],
   imports: [
+   
     BrowserModule,
     AppRoutingModule
+  ],
+  exports:[
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
